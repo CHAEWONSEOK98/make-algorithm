2c5812385d643972828f5ac9aaad06ecbce6735d
CHAEWONSEOK98 <83595773+CHAEWONSEOK98@users.noreply.github.com>
2022-01-18 13:29:16 +0900

    Update 2.js

--- 연습/기본문제/2.js
+++ 연습/기본문제/2.js
@@ -6,3 +6,18 @@ function solution(a, b, c) {
 }
 
 console.log(solution(13, 33, 17));
+
+/*
+function solution(a,b,c) {
+  let answer = "YES";
+  let max;
+  let sum = a+b+c;
+  if(a>b) max = a;
+  else max = b;
+  if(c>max) max = c;
+  if((sum-max)<=max) answer="NO";
+  return answer;
+}
+
+console.log(solution(13, 33, 17));
+*/
