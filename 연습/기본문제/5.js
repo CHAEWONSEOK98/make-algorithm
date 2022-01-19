ce607bbadcff5e487ec184aa783789d967a90f1f
CHAEWONSEOK98 <83595773+CHAEWONSEOK98@users.noreply.github.com>
2022-01-19 15:32:44 +0900

    Update 5.js

--- 연습/기본문제/5.js
+++ 연습/기본문제/5.js
@@ -43,3 +43,23 @@ function solution(arr){
 let arr=[5,7,1,3,2,9,11];
 console.log(solution(arr))
 */
+
+/*
+function solution(arr){
+  let answer = Math.min(...arr);
+  return answer;
+}
+
+let arr=[5,7,1,3,2,9,11];
+console.log(solution(arr))
+*/
+
+/*
+function solution(arr){
+  let answer = Math.min.apply(null, arr);
+  return answer;
+}
+
+let arr=[5,7,1,3,2,9,11];
+console.log(solution(arr))
+*/
