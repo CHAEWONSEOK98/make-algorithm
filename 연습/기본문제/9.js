25288653ded03894e55c5f3010ca05d8dfa185d9
CHAEWONSEOK98 <7dugod7@naver.com>
2022-01-22 02:13:18 +0900

    9.js

---
+++ 연습/기본문제/9.js
@@ -0,0 +1,19 @@
+// A를 #으로
+/*대문자로 이루어진 영어단어가 입력되면 단어에 포함된 ‘A'를 모두 ’#‘으로 바꾸어 출력하는 
+프로그램*/
+
+function solution(str) {
+  const answer = str;
+  const arr = [];
+  for (let i = 0; i < answer.length; i++) {
+    if (answer[i] === "A") {
+      arr.push("#");
+      continue;
+    }
+    arr.push(answer[i]);
+  }
+  return arr.join("");
+}
+
+const str = String("BANANA");
+console.log(solution(str));
