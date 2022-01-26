601188a6bf9ad3460382491bf6a77619d0cd4289
CHAEWONSEOK98 <7dugod7@naver.com>
2022-01-26 14:03:55 +0900

    updated 15.js 16.js

--- 연습/기본문제/15.js
+++ 연습/기본문제/15.js
@@ -16,3 +16,27 @@ function solution(str) {
 
 let str = String("study");
 console.log(solution(str));
+
+/*
+function solution(s) {
+  let answer;
+  let mid=Math.floor(s.length/2);
+  if(s.length%2==1) answer = s.substring(mid, mid+1);
+  else answer = s.substring(mid-1, mid+1);
+  return answer;
+}
+
+console.log(solution('good'));
+*/
+
+/*
+function solution(s) {
+  let answer;
+  let mid=Math.floor(s.length/2);
+  if(s.length%2==1) answer = s.substr(mid, 1);
+  else answer = s.substr(mid-1, 2);
+  return answer;
+}
+
+console.log(solution('good'));
+*/
--- 연습/기본문제/16.js
+++ 연습/기본문제/16.js
@@ -20,3 +20,32 @@ const set = new Set(dupArr);
 const uniqueArr = Array.from(set);
 console.log(uniqueArr) //[1,2,3]
 */
+
+/*
+function solution(s) {
+  let answer='';
+  //console.log(s.indexOf('k')):
+  for(let i = 0; i<s.length; i++){
+    //console.log(s[i], i, s.indexOf(s[i]));
+    if(s.indexOf(s[i]===i) answer+=s[i]);
+  }
+  return answer;
+}
+
+console.log(solution("ksekkset"));
+*/
+
+//특정 문자 찾기
+/*
+function solution(s) {
+  let answer = 0;
+  let pos = s.indexOf('k');
+  while(pos!==-1){
+    answer++;
+    pos = s.indexOf('k', pos+1);
+  }
+  return answer;
+}
+
+console.log(solution("ksekkset"));
+*/
