001aabed9ff3fcded100b38e1845355b83998dc8
CHAEWONSEOK98 <7dugod7@naver.com>
2022-01-26 14:03:08 +0900

    17.js

---
+++ 연습/기본문제/17.js
@@ -0,0 +1,13 @@
+//중복단어제거
+/*
+N개의 문자열이 입력되면 중복된 문자열은 제거하고 출력하는 프로그램을 작성하세요.
+출력하는 문자열은 원래의 입력순서를 유지합
+*/
+
+function solution(arr) {
+  const set = new Set(arr);
+  return set;
+}
+
+const arr = ["good", "time", "good", "time", "student"];
+console.log(solution(arr));
