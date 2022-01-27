dadc2497cb2f94c5ec426fd433ecccf47713d840
CHAEWONSEOK98 <7dugod7@naver.com>
2022-01-27 14:28:40 +0900

    1.js

---
+++ 연습/1, 2차원 배열 탐색/1.js
@@ -0,0 +1,18 @@
+//큰 수 출력하기
+/*
+N(1<=N<=100)개의 정수를 입력받아, 자신의 바로 앞 수보다 큰 수만 출력하는 프로그램
+(첫 번째 수는 무조건 출력한다)
+*/
+
+function solution(arr) {
+  let brr = [];
+  brr.push(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i + 1]) brr.push(arr[i + 1]);
+  }
+  return brr;
+}
+
+const Number = 6;
+const arr = [7, 3, 9, 5, 6, 12];
+console.log(solution(arr));
