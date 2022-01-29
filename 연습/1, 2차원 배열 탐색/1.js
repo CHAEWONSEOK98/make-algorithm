82850de83ef4e241dd8d801f341b629239497169
CHAEWONSEOK98 <7dugod7@naver.com>
2022-01-29 22:33:27 +0900

    Updated 1, 2.js

--- 연습/1, 2차원 배열 탐색/1.js
+++ 연습/1, 2차원 배열 탐색/1.js
@@ -16,3 +16,16 @@ function solution(arr) {
 const Number = 6;
 const arr = [7, 3, 9, 5, 6, 12];
 console.log(solution(arr));
+
+/*
+function solution(arr){
+  let answer = [];
+  answer.push(arr[0]);
+  for(let i = 1; arr.length; i++){
+    if(arr[i] > arr[i-1]) answer.push(arr[i]);
+  }
+  return answer;
+}
+const arr = [7, 3, 9, 5, 6, 12];
+console.log(solution(arr));
+*/
--- 연습/1, 2차원 배열 탐색/2.js
+++ 연습/1, 2차원 배열 탐색/2.js
@@ -21,3 +21,19 @@ function solution(arr) {
 const Number = 8;
 const arr = [130, 135, 148, 140, 145, 150, 150, 153];
 console.log(solution(arr));
+
+/*
+function solution(arr) {
+  let answer=1, max=arr[0];
+  for(let i=1; i<arr.length; i++){
+    if(arr[i]>max) {
+      answer++;
+      max=arr[i];
+    }
+  }
+  return answer;
+}
+
+const arr = [130, 135, 148, 140, 145, 150, 150, 153];
+console.log(solution(arr));
+*/
