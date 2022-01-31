37e39186aeb5b32336ae21219b339549fdf95711
CHAEWONSEOK98 <7dugod7@naver.com>
2022-02-01 04:45:48 +0900

    updated 4.js

--- 연습/1, 2차원 배열 탐색/4.js
+++ 연습/1, 2차원 배열 탐색/4.js
@@ -30,5 +30,22 @@ console.log(solution(arr));
 연속으로 3번 등장하면 3점.
 가령 1 2 3 번에 각각 1이 있으면 1, 2, 3 으로 총 6점 획득
 메인으로 카운트해주는 변수랑 이걸 보조해주는 변수가 필요하다고 생각함.
+*/
 
+/*
+function solution(arr) {
+  let cnt = 0;
+  let answer = 0;
+  for(let i = 0; i<arr.length; i++){
+    if(arr[i]===1){
+      cnt++;
+      answer+=cnt;
+    }
+    else cnt = 0;
+  }
+  return answer;
+}
+
+const arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+console.log(solution(arr));
 */
