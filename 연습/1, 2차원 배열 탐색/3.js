2bbd314e39c11c3f9cdc353528e5570072ecd8f4
CHAEWONSEOK98 <7dugod7@naver.com>
2022-02-01 05:11:53 +0900

    updated 3.js

--- 연습/1, 2차원 배열 탐색/3.js
+++ 연습/1, 2차원 배열 탐색/3.js
@@ -1,5 +1,4 @@
 function solution(arr, brr) {
-  console.log(arr, brr);
   let result = [];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === 1) {
@@ -34,3 +33,21 @@ function solution(arr, brr) {
 const arr = [2, 3, 3, 1, 3];
 const brr = [1, 1, 2, 2, 3];
 console.log(solution(arr, brr));
+
+/*
+function solution(a, b) {
+  let answer='';
+  for(let i = 0; i<a.length; i++){
+    if(a[i]===b[i]) answer += 'D'+' ';
+    else if(a[i]===1 && b[i]===3) answer += 'A'+' ';
+    else if(a[i]===2 && b[i]===1) answer += 'A'+' ';
+    else if(a[i]===3 && b[i]===2) answer += 'A'+' ';
+    else answer+= 'B'+' ';
+  }
+  return answer;
+}
+
+const a = [2, 3, 3, 1, 3];
+const b = [1, 1, 2, 2, 3];
+console.log(solution(a, b));
+*/
