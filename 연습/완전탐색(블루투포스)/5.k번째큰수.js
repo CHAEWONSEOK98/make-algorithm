9c6bd24e4a1fa42cb4d5d1681795eac6ce3dfd36
CHAEWONSEOK98 <7dugod7@naver.com>
2022-05-13 11:20:32 +0900

    22.05.13

--- 연습/완전탐색(블루투포스)/5.k번째큰수.js
+++ 연습/완전탐색(블루투포스)/5.k번째큰수.js
@@ -1,6 +1,16 @@
 function solution(n, k, arr) {
   let answer;
-
+  let store = new Set();
+  for (let i = 0; i < n - 2; i++) {
+    for (let j = i + 1; j < n - 1; j++) {
+      for (let k = j + 1; k < n; k++) {
+        store.add(arr[i] + arr[j] + arr[k]);
+      }
+    }
+  }
+  //Set객체는 sort가 안됨. 그래서 배열로 바꿔주고 정렬.
+  let q = Array.from(store).sort((a, b) => b - a);
+  answer = q[k - 1];
   return answer;
 }
 
