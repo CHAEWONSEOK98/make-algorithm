4b021290ae49ed5788ba7132dfa633b17fc16058
CHAEWONSEOK98 <7dugod7@naver.com>
2022-07-05 07:24:41 +0900

    '두배열합치기(Two-Pointers-Algorithm)'

---
+++ 연습/효율성/1.두배열합치기.js
@@ -0,0 +1,26 @@
+function solution(arr1, arr2) {
+  let answer = [];
+  let n = arr1.length;
+  let m = arr2.length;
+  let p1 = (p2 = 0);
+  while (p1 < n && p2 < m) {
+    if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
+    else answer.push(arr2[p2++]);
+  }
+  while (p1 < n) answer.push(arr1[p1++]);
+  while (p2 < m) answer.push(arr2[p2++]);
+
+  return answer;
+}
+
+let a = [1, 3, 5];
+let b = [2, 3, 6, 7, 9];
+console.log(solution(a, b));
+
+// const a = [1, 3, 5];
+// const b = [2, 3, 6, 7, 9];
+// let c = [...a, ...b];
+
+// c.sort(function (a, b) {
+//   return a - b;
+// });
