1e7104c21df209b669f69df73d20d0a1269c68fa
CHAEWONSEOK98 <7dugod7@naver.com>
2023-03-28 19:16:52 +0900

    singly-linked-list : get, set, insert, remove, reverse

--- js-algorithm/data-structure/singly-linked-list/singly-linked-list.js
+++ js-algorithm/data-structure/singly-linked-list/singly-linked-list.js
@@ -102,6 +102,39 @@ class SinglyLinkedList {
     this.length--;
     return removed;
   }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    for(let i = 0; i< this.length; i++){
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
+  print() {
+    let arr = [];
+    let current = this.head;
+    while(current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
 }
 
 let list = new SinglyLinkedList();
+
+Big O of Singly Linked Lists
+
+Insertion - O(1)
+Removal - O(1) or O(N)
+Searching - O(N)
+Access - O(N)
+
+삽입, 삭제
+Array < SinglyLinkedList
