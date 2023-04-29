14bca97b16a8976c52d23755c970bebad13eee88
CHAEWONSEOK98 <7dugod7@naver.com>
2023-04-30 01:56:18 +0900

    Queue

--- js-algorithm/data-structure/stacks-queues/stacks-queues.js
+++ js-algorithm/data-structure/stacks-queues/stacks-queues.js
@@ -44,3 +44,49 @@ class Stack {
     return temp.value;
   }
 }
+
+// array + queue : [], unshift, pop
+// 삽입과 제거는 상수값의 시간을 가져야한다.
+// BIG O:
+-Insertion - O(1) - Removal - O(1) - Searching - O(N) - Access - O(N);
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(value) {
+    let newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode; // pointer 작업
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+let q = new Queue();
+q.enqueue();
+q.dequeue();
