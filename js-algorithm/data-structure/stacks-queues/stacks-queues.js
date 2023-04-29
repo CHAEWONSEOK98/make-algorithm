a801f72a3231dee100c002b264178e981285a8e7
CHAEWONSEOK98 <7dugod7@naver.com>
2023-04-29 22:50:07 +0900

    stack

---
+++ js-algorithm/data-structure/stacks-queues/stacks-queues.js
@@ -0,0 +1,46 @@
+// array + stack : [], push, pop
+
+// singly-linked-list + stack
+// stack에서는 push와 pop이 상수값의 시간을 가져야한다.
+// stack BIG O:
+//  - Insertion - O(1)
+//  - Removal - O(1)
+//  - Searching - O(N)
+//  - Access - O(N)
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+  pop() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
