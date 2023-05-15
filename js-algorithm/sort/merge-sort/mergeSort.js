6340ad8f36e35818b58428f6e1f8c3d590edd86e
CHAEWONSEOK98 <7dugod7@naver.com>
2023-05-15 21:12:37 +0900

    Merge Sort

---
+++ js-algorithm/sort/merge-sort/mergeSort.js
@@ -0,0 +1,45 @@
+// 세 가지 조합이라고 볼 수 있다. 분할, 정렬, 합병
+// Big O
+// 배열이 늘어나는 비율 : log n
+//  분할 한 후 합병을 수행할 때 : O(n) 비교가 필요
+// 따라서 Big O = O(n log n)
+
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  console.log(results);
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+
+  return results;
+}
+
+console.log(merge([1, 10, 50], [2, 14, 99, 100]));
+
+ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ;
+// recursion
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+console.log(mergeSort([10, 24, 76, 73, 72, 1, 9]));
