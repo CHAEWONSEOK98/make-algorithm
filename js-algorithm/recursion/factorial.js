77af0a07b7b37da45aa2e47b5169442b45776a97
CHAEWONSEOK98 <7dugod7@naver.com>
2023-05-16 17:50:00 +0900

    factorial-edit

--- js-algorithm/recursion/factorial.js
+++ js-algorithm/recursion/factorial.js
@@ -4,8 +4,8 @@
 // factorial(7) // 5040
 
 function factorial(n) {
-    if(n === 0) return 1;
-    return n * factorial(n -)
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
 }
 
 console.log(factorial(4));
