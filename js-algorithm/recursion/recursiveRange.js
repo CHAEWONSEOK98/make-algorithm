ebc5f28e296eb5e0fd8126453e2d65717fa8bd86
CHAEWONSEOK98 <7dugod7@naver.com>
2023-05-16 17:54:40 +0900

    recursion-recursiveRange

---
+++ js-algorithm/recursion/recursiveRange.js
@@ -0,0 +1,9 @@
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55
+
+function recursiveRange(n) {
+  if (n === 1) return 1;
+  return n + recursiveRange(n - 1);
+}
+
+console.log(recursiveRange(100));
