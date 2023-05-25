0f65451ab871257047a66a0318a78649594a8256
CHAEWONSEOK98 <7dugod7@naver.com>
2023-05-26 06:10:08 +0900

    binary-tree-traversal DFSPostOrder

--- js-algorithm/data-structure/trees/tree-traversal/binary-tree-traversal.js
+++ js-algorithm/data-structure/trees/tree-traversal/binary-tree-traversal.js
@@ -93,9 +93,18 @@ class BinarySearchTree {
         if(node.left) traverse(node.left); 
         if(node.right) traverse(node.right);
     }
-    
     traverse(this.root);
     return data; // [10, 6, 3, 8, 15, 20]
+  }
+  DFSPostOrder() {
+    let data = [];
+    function traverse(node) {
+        if(node.left) traverse(node.left);
+        if(node.right) traverse(node.right);
+        data.push(node.value);
+    }
+    traverse(this.root);
+    return data; // [3, 8, 6, 20, 15, 10];
   }
 }
 
