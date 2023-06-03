7301ac282cef353b2db092322a1acc5f2b622628
CHAEWONSEOK98 <7dugod7@naver.com>
2023-06-03 12:30:06 +0900

    폰켓몬

---
+++ programmers/Lv.1/폰켓몬.js
@@ -0,0 +1,14 @@
+function solution(nums) {
+  let total = nums.length / 2;
+  let set = new Set(nums);
+  let arr = [...set];
+  let cnt = 0;
+  for (let i = 0; i < total; i++) {
+    arr.pop();
+    cnt += 1;
+    if (arr.length === 0) return cnt;
+  }
+  return cnt;
+}
+
+console.log(solution([3, 3, 3, 2, 2, 4]));
