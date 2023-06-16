3f7965cab4f57a2cfd33ef14c891aa0fdb07fa5c
CHAEWONSEOK98 <7dugod7@naver.com>
2023-06-16 20:57:37 +0900

    공통원소구하기

---
+++ 연습/효율성/2.공통원소구하기.js
@@ -0,0 +1,17 @@
+function solution(arr1, arr2) {
+  let result = [];
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+  let p1 = (p2 = 0);
+
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] === arr2[p2]) {
+      result.push(arr1[p1++]);
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) p1++;
+    else p2++;
+  }
+  return result;
+}
+
+console.log(solution([1, 3, 10, 5, 2], [3, 2, 5, 7, 8]));
