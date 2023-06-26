9b9579c8b0b813295805f529cb6ff5e805b42aaa
CHAEWONSEOK98 <7dugod7@naver.com>
2023-06-27 01:44:56 +0900

    fix - 일곱난쟁이

--- 연습/기본문제/8.일곱난쟁이.js
+++ 연습/기본문제/8.일곱난쟁이.js
@@ -16,23 +16,27 @@
 
 function solution(arr) {
   const answer = arr;
-  const p = answer.reduce(function (acc, v) {
+  const sum = answer.reduce(function (acc, v) {
     return acc + v;
   });
+  let flag = 0;
 
   for (let i = 0; i < 8; i++) {
     for (let j = 1; j < 9; j++) {
-      if (p - answer[i] - answer[j] === 100) {
+      if (sum - answer[i] - answer[j] === 100) {
         answer.splice(j, 1);
         answer.splice(i, 1);
-        return answer;
+        flag = 1;
+        break;
       }
     }
+    if (flag === 1) break;
   }
+
+  return answer;
 }
 
-const arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
-console.log(solution(arr));
+console.log(solution([20, 7, 23, 19, 10, 15, 25, 8, 13]));
 
 /*
 function solution(arr) {
